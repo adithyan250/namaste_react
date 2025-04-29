c1048f21bcb728e087441ee622723eee8ef4ef65
Adithyan PB <adithyanpb2005@gmail.com>
2025-04-29 17:41:46 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
 const parent = React.createElement("div", {
     id: "parent"
 }, React.createElement("div", {
     id: "child"
 },[
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an h1 tag!!!!"),
     React.createElement("h2", {}, "i'm a h2 tag")
 ]));
 
