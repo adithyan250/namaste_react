b8d110d412710bdaf9f5faa128fb3c8a6409e65c
Adithyan PB <adithyanpb2005@gmail.com>
2025-06-20 21:04:19 +0530

    episode 10

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,9 +13,9 @@ const RestaurantCard = (props) => {
     const {deliveryTime} = resData.info.sla
 
   return (
-    <div className="res-card" style={{backgroundColor: "#f0f0f0",}}>
-        <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
-      <h3>{name}</h3>
+    <div className="m-4 p-4 w-[245] rounded-xl hover:bg-gray-100">
+        <img className="rounded-xl" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} star</h4> 
       <h4>{costForTwo}</h4>
